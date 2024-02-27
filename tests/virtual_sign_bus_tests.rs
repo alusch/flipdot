e453fdb3e14a18485c2162a5de16456c867cc5e0
Adam Lusch <alusch@gmail.com>
2024-02-27 09:33:24 -0600

    Test auto flipping sign-vsign interaction

--- tests/virtual_sign_bus_tests.rs
+++ tests/virtual_sign_bus_tests.rs
@@ -8,7 +8,7 @@ use flipdot_testing::{VirtualSign, VirtualSignBus};
 #[test]
 fn sign_virtual_sign_interaction() {
     let bus = VirtualSignBus::new(vec![
-        VirtualSign::new(Address(3), PageFlipStyle::Manual),
+        VirtualSign::new(Address(3), PageFlipStyle::Automatic),
         VirtualSign::new(Address(6), PageFlipStyle::Manual),
     ]);
     let bus = Rc::new(RefCell::new(bus));
@@ -64,6 +64,36 @@ fn sign_virtual_sign_interaction() {
     assert!(bus.borrow().sign(0).pages().is_empty());
     assert_eq!(State::Unconfigured, bus.borrow().sign(0).state());
     assert_eq!(&pages2, bus.borrow().sign(1).pages());
+    assert_eq!(State::PageLoaded, bus.borrow().sign(1).state());
+
+    let sign2 = Sign::new(bus.clone(), Address(3), SignType::Max3000Dash30x7);
+    sign2.configure().unwrap();
+
+    // Configuring sign 3 should not have affected sign 6.
+    assert_eq!(State::ConfigReceived, bus.borrow().sign(0).state());
+    assert_eq!(State::PageLoaded, bus.borrow().sign(1).state());
+
+    let mut pages3 = [sign2.create_page(PageId(1))];
+    pages3[0].set_pixel(2, 3, true);
+    sign2.send_pages(&pages3).unwrap();
+
+    // After sending pages to sign 3, the corresponding virtual sign should be in the PageLoaded
+    // state with the appropriate page list, and the other should be unaffected.
+    assert_eq!(&pages3, bus.borrow().sign(0).pages());
+    assert_eq!(State::ShowingPages, bus.borrow().sign(0).state());
+    assert_eq!(&pages2, bus.borrow().sign(1).pages());
+    assert_eq!(State::PageLoaded, bus.borrow().sign(1).state());
+
+    sign2.show_loaded_page().unwrap();
+
+    // These functions should be no-ops for automatic flip signs.
+    assert_eq!(State::ShowingPages, bus.borrow().sign(0).state());
+    assert_eq!(State::PageLoaded, bus.borrow().sign(1).state());
+
+    sign2.load_next_page().unwrap();
+
+    // These functions should be no-ops for automatic flip signs.
+    assert_eq!(State::ShowingPages, bus.borrow().sign(0).state());
     assert_eq!(State::PageLoaded, bus.borrow().sign(1).state());
 
     sign.shut_down().unwrap();
